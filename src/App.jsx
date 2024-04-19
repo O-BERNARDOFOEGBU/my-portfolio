3e2749900b15b675aabfa68eedccc21e7b83be27
O-Bernard Ofoegbu <obinnaofoegbu71@gmail.com>
2024-04-19 14:41:52 +0100

    start

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import { useEffect } from 'react';
-import Layout from './components/Layout/Layout';
-import Home from './pages/Home';
-import BlogDetails from './components/Blog/BlogDetails';
-import Aos from 'aos';
-import 'aos/dist/aos.css';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { useEffect } from "react";
+import Layout from "./components/Layout/Layout";
+import Home from "./pages/Home";
+import BlogDetails from "./components/Blog/BlogDetails";
+import Aos from "aos";
+import "aos/dist/aos.css";
 
 function App() {
   useEffect(() => {
