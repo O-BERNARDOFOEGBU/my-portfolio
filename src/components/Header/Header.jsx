95c8c40c15b950b06d3a00bf550e1f84076fed35
O-Bernard Ofoegbu <obinnaofoegbu71@gmail.com>
2024-05-22 15:19:05 +0100

    add-work: Sabi

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -24,7 +24,7 @@ const Header = () => {
                 {" "}
                 <Link className="st-site-branding" to="/" id="hero">
                   {/* <img src="/images/logo.png" alt="Davis" /> */}
-                  <img src="./images/favicon.ico" alt="Ofoegbu's Logo" />
+                  <img src="./images/favicon.ico" alt="Ofoegbu" />
                 </Link>
               </span>
               <span className="st-hero-text-header">foegbu</span>
