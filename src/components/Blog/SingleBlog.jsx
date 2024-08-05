2ad92984a02325fbefbb7f811ed1136ffd0c9998
O-Bernard Ofoegbu <obinnaofoegbu71@gmail.com>
2024-08-05 18:38:29 +0100

    blog fixed

--- src/components/Blog/SingleBlog.jsx
+++ src/components/Blog/SingleBlog.jsx
@@ -1,25 +1,33 @@
-import { Link } from 'react-router-dom';
-import './Blog.scss';
-import PropTypes from 'prop-types';
+import { Link } from "react-router-dom";
+import "./Blog.scss";
+import PropTypes from "prop-types";
 
 const SingleBlog = ({ element }) => {
   const { imgLink, title, date, designation, href } = element;
   return (
     <div className={`st-post-single st-style1`}>
-      <Link to={href} className="st-post-thumb st-zoom">
+      <Link
+        style={{ width: "370px", height: "221px", padding: "5px" }}
+        to={href}
+        className="st-post-thumb st-zoom"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
         <img src={imgLink} className="st-zoom-in" alt="blog1" />
       </Link>
       <div className="st-post-info">
         <div className="st-post-date">
           By:
           <a href="blog-details.html" className="st-post-author">
-            {designation}
+            {` ${designation}`}
           </a>
           <span className="st-post-date-divider">|</span>
           <span className="st-post-publish-date">{date}</span>
         </div>
         <h4 className="st-post-title">
-          <Link to={href}>{title}</Link>
+          <Link to={href} target="_blank" rel="noopener noreferrer">
+            {title}
+          </Link>
         </h4>
       </div>
     </div>
