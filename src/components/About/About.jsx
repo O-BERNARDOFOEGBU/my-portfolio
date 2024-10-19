86128e9d7758f05970c7ac5c77f9bf1c5774ad35
O-Bernard Ofoegbu <obinnaofoegbu71@gmail.com>
2024-10-19 18:24:14 +0100

    seo optimization

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,6 +1,6 @@
-import PropTypes from 'prop-types';
-import './About.scss';
-import SectionHeading from '../SectionHeading/SectionHeading';
+import PropTypes from "prop-types";
+import "./About.scss";
+import SectionHeading from "../SectionHeading/SectionHeading";
 
 const About = ({ data }) => {
   const { imgLink, title, subtitle, text, details, cvPdf } = data;
@@ -12,14 +12,26 @@ const About = ({ data }) => {
         <div className="row">
           <div className="col-lg-6 ">
             <div className="st-about-img-wrap">
-              <div className="st-about-img st-bg" style={{ backgroundImage: `url(${imgLink})`}} data-aos="fade-right" data-aos-duration="800" data-aos-delay="400"></div>
+              <div
+                className="st-about-img st-bg"
+                style={{ backgroundImage: `url(${imgLink})` }}
+                alt="Bernard Ofoegbu"
+                data-aos="fade-right"
+                data-aos-duration="800"
+                data-aos-delay="400"
+              ></div>
             </div>
             <div className="st-height-b0 st-height-lg-b30"></div>
           </div>
           <div className="col-lg-6">
             <div className="st-vertical-middle">
               <div className="st-vertical-middle-in">
-                <div className={`st-text-block st-style1`} data-aos="zoom-in" data-aos-duration="1000" data-aos-delay="500">
+                <div
+                  className={`st-text-block st-style1`}
+                  data-aos="zoom-in"
+                  data-aos-duration="1000"
+                  data-aos-delay="500"
+                >
                   <h2 className="st-text-block-title">{title}</h2>
                   <h4 className="st-text-block-subtitle">{subtitle}</h4>
                   <div className="st-text-block-text">
@@ -33,7 +45,13 @@ const About = ({ data }) => {
                     ))}
                   </ul>
                   <div className="st-text-block-btn">
-                    <a className='st-btn st-style1 st-color1' href={cvPdf} download>Download CV</a>
+                    <a
+                      className="st-btn st-style1 st-color1"
+                      href={cvPdf}
+                      download
+                    >
+                      Download CV
+                    </a>
                   </div>
                 </div>
               </div>
@@ -42,14 +60,11 @@ const About = ({ data }) => {
         </div>
       </div>
     </section>
-  )
-}
+  );
+};
 
 About.propTypes = {
-  data: PropTypes.object
-}
+  data: PropTypes.object,
+};
 
 export default About;
-
-
-
