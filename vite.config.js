7b5df99f318e46c6f9755213367f9217b8361afa
O-BERNARDOFOEGBU <obinnaofoegbu71@gmail.com>
2025-11-06 19:24:46 +0100

    portfolio added

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,17 @@
+// import { defineConfig } from "vite";
+// import react from "@vitejs/plugin-react";
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   // base: "/my-portfolio/",
+//   base: "/",
+// });
+
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // base: "/my-portfolio/",
-  base: "/",
+  base: "/my-portfolio/",
 });
